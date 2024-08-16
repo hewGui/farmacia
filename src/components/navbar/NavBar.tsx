bc6747f5603c9c33312ddd58f423d6914f4ff05a
Guilherme <guilhermesoaresilvat@gmail.com>
2024-08-16 19:12:32 -0300

    Componentes Iniciais

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar-container">
+                <div className="navbar-content">
+                    <div className="navbar-brand">DrogaMais</div>
+                    <div className="navbar-links">
+                        <Link to="/home" className="navbar-link">Home</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
